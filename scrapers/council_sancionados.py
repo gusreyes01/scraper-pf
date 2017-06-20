2dd548ca2ee8dd4e33926dfd2dbb9b8753c87f4e
Francisco Pantoja <fcopantoja@gmail.com>
2017-06-19 20:17:06 -0500

    Added Screening List scraper working from internal tables

--- scrapers/council_sancionados.py
+++ scrapers/council_sancionados.py
@@ -2,6 +2,7 @@ import requests
 
 from bs4 import BeautifulSoup
 from db import get_connection
+from scrapers.scraper import WebScraper, log_decorator
 
 
 def get_sancionados():
@@ -36,5 +37,27 @@ def get_sancionados():
     finally:
         conn.close()
 
+
+class CouncilSancionadosScraper(WebScraper):
+    def __init__(self):
+        super(CouncilSancionadosScraper, self).__init__()
+        self.agent_id = 15
+
+    @log_decorator
+    def execute(self, q=None):
+        super(CouncilSancionadosScraper, self).execute()
+
+        query = "SELECT id, description, url FROM council_sancionados WHERE description LIKE '%{}%';".format(q)
+        self.cursor.execute(query)
+        query_params = [(record[1][:255], record[2], record[1][:255], self.agent_id) for record in self.cursor]
+
+        for params in query_params:
+            self.cursor.execute('INSERT INTO scraper (header, url, body, fuente_id) VALUES (%s, %s, %s, %s);', params)
+            self.incr()
+
+        self.conn.commit()
+
+
 if __name__ == '__main__':
-    get_sancionados()
+    CouncilSancionadosScraper().execute(q='DPRK Ministry of State')
+    # get_sancionados()
